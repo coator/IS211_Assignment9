5495f35cdee8072af8f0f3f7fbb34aa5e6a33390
gamblingrepairman <36687218+gamblingrepairman@users.noreply.github.com>
2020-10-16 18:33:12 -0400

    fixed issue with bs4 install, Worked on part one

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+r = requests.get("https://www.cbssports.com/nfl/stats/player/scoring/nfl/regular/qualifiers")
 
+soup = BeautifulSoup(r.text, 'html.parser')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+d = soup.find_all('tr', {"class": 'TableBase-bodyTr'})
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for dd in d:
+    print(dd)
