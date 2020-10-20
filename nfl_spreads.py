c6e6f8daffe662955c496d9aced84442f9f199b2
gamblingrepairman <awaite67@gmail.com>
2020-10-20 05:08:12 -0400

    a lot has occurred

--- nfl_spreads.py
+++ nfl_spreads.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import re
 import json
 
 
@@ -11,7 +12,7 @@ class FParams:
         else:
             self.current_working_folder = "/website_download/" + fn + "/"
         self.current_working_file = fn + ".htm"
-        self.localpath = os.getcwd()+self.current_working_folder
+        self.localpath = os.getcwd() + self.current_working_folder
         self.htmfileloc = self.localpath + self.current_working_file
 
     def SearchDir(self):
@@ -29,9 +30,36 @@ def htm_pull(files, url):
 
 
 def parse_file(files):
+    a, aa = [], []
     r = open(files.htmfileloc, encoding="utf-8")
-    for rr in r:
-        print(rr)
+    soup = BeautifulSoup(r, "html.parser")
+    re_str = "<TR>\n\s\s\s\s<TD>.*<\/TD>\n\s\s\s\s<TD>.*<\/TD>\n\s\s\s\s<TD>.*<\/TD>\n\s\s\s\s<TD>.*<\/TD>\n\s\s<\/TR>"
+
+    for item in soup(text=re.compile(re_str)):
+        a.append(item)
+
+    for item in a:
+        g = re.findall(re_str, item)
+        for i in g:
+            i = i.replace('</TR>', '').replace('<TR>', '').replace('</TD>', ',').replace('<TD>', '')\
+                .replace('\n    ',' ').replace('\n  ',' ')
+            aa.append(i)
+    """    json_str = {"Date": item[0],
+                    "TeamA": item[1],
+                    "Handicapping score": item[2],
+                    "TeamB": item[3]}
+        json_out = json.dumps(json_str)
+        handicapps.append(json_out)"""
+    handicapps=[]
+    for item in aa:
+        item = item.split(',')
+        json_str = {"Date": item[0],
+                    "TeamA": item[1],
+                    "Handicapping score": item[2],
+                    "TeamB": item[3]}
+        json_out = json.dumps(json_str)
+        handicapps.append(json_out)
+    for x in handicapps: print(x)
 
 
 def main():
